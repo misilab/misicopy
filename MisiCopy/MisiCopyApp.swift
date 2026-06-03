bb2692e0a8973cace4340fa9c1ba096336dceef1
Misiraca <contact@misiraca.com>
2026-06-03 22:12:12 +0200

    Initial Commit

---
+++ MisiCopy/MisiCopyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MisiCopyApp.swift
+//  MisiCopy
+//
+//  Created by Matthieu Misiraca on 03/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct MisiCopyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
